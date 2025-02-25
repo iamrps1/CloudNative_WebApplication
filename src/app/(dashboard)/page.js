9e68a88617ce06f3f68f13e74021d34f13a70b95
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2025-02-26 02:22:26 +0530

    feat: added new card component and dynamically adding it

--- src/app/(dashboard)/page.js
+++ src/app/(dashboard)/page.js
@@ -1,11 +1,61 @@
-import Image from "next/image"
+import { DashboardCard } from "@/components/ui/cards"
+import { cn } from "@/lib/utils"
+import { Home, Info, Mail, User } from "lucide-react"
+
+const links = [
+    {
+        id: 1,
+        name: "Home",
+        href: "/",
+        icon: <Home size={40} />,
+        bgColor: "bg-sky-700",
+        heading: "Check Authenticity",
+        paragraph: "Check the authenticity of your product",
+    },
+    {
+        id: 2,
+        name: "About",
+        href: "/about",
+        icon: <Info size={40} />,
+        bgColor: "bg-orange-500",
+        heading: "Check Authenticity",
+        paragraph: "Check the authenticity of your product",
+    },
+    {
+        id: 3,
+        name: "Contact",
+        href: "/contact",
+        icon: <Mail size={40} />,
+        bgColor: "bg-rose-500",
+        heading: "Check Authenticity",
+        paragraph: "Check the authenticity of your product",
+    },
+    {
+        id: 4,
+        name: "Dashboard",
+        href: "/dashboard",
+        icon: <User size={40} />,
+        bgColor: "bg-violet-500",
+        heading: "Check Authenticity",
+        paragraph: "Check the authenticity of your product",
+    },
+]
 
 const MainDashboard = () => {
-    return <div>Main Dashboard
+    return (
         <div>
-            <Image src='/dashboard.jpeg' className="w-full" width={500} height={300} alt="Main Dashboard" />
+            <h1 className="text-3xl font-bold my-5">Welcome to the CopySure - Authentic Checker</h1>
+
+            <div className="my-5 grid grid-cols-3 gap-6">
+                {links.map((link) => (
+                    <DashboardCard key={link.id} icon={link.icon} className={cn(link.bgColor)}>
+                        <h3 className="text-xl font-bold mb-2">{link.heading}</h3>
+                        <p>{link.paragraph}</p>
+                    </DashboardCard>
+                ))}
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default MainDashboard
